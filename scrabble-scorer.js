42caef4bf012aaf7bc8db632c53374973b15793d
EricGlover <ericbglover@att.net>
2021-03-17 20:14:03 +0000

    Functions appear complete. Top-level function calls are not ready.

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -13,46 +13,133 @@ const oldPointStructure = {
 };
 
 function oldScrabbleScorer(word) {
-	word = word.toUpperCase();
 	let letterPoints = "";
+  word = word.toUpperCase();
  
 	for (let i = 0; i < word.length; i++) {
- 
+
 	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
- 
+		  if (oldPointStructure[pointValue].includes(word[i])) {
+			  letterPoints += `Points for '${word[i]}': ${pointValue}\n`;
+        scrabbleScore = scrabbleScore + pointValue;
+		  }
 	  }
 	}
+
 	return letterPoints;
- }
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
    console.log("Let's play some scrabble! Enter a word:");
+
+   let scrabbleWord = input.question("Please enter a word ");
+   console.log(oldScrabbleScorer(scrabbleWord));
 };
 
-let simpleScore;
 
-let vowelBonusScore;
+function simpleScore(word) {
+  let letterPoints = "";
 
-let scrabbleScore;
+  for (let i = 0; i < word.length; i++) {
+    letterPoints += `Points for '${word[i]}': 1\n`;
+    scrabbleScore++;
+  }
 
-const scoringAlgorithms = [];
+  return letterPoints;
+}
 
-function scorerPrompt() {}
 
-function transform() {};
+function vowelBonusScore(word) {
+  let letterPoints = "";
+  word = word.toUpperCase();
 
-let newPointStructure;
+  for (i = 0; i < word.length; i++) {
+    if (word[i] === 'A' || word[i] === 'E' || word[i] === "I" || word[i] === "O" || word[i] === "U") {
+      letterPoints += `Points for '${word[i]}': 3\n`;
+      scrabbleScore = scrabbleScore + 3;
+
+    } else {
+      letterPoints += `Points for '${word[i]}': 1\n`;
+      scrabbleScore++;
+    }
+  }
+
+  return letterPoints;
+}
+
+
+let scrabbleScore = 0;
+
+const scoringAlgorithms = [
+  {
+    Name: "Simple Score",
+    Description: "Each letter is worth 1 point.",
+    ScoreFunction: "A function with a parameter for user input that returns a score."
+  }, {
+    Name: "Bonus Vowels",
+    Description: "Vowels are 3 pts, consonants are 1 pt.",
+    ScoreFunction: "A function that returns a score based on the number of vowels and consonants."
+  }, {
+    Name: "Scrabble",
+    Description: "The traditional scoring algorithm.",
+    ScoreFunction: "Uses the oldScrabbleScorer() function to determine the score for a given word."
+  }
+];
+
+function scorerPrompt() {
+  
+  let scorerInput = input.question("Which scoring algorithm would you like to use? 0 for simple scorer, 1 for vowel bonus scorer, 2 for scrabble scorer");
+  let valid = false;
+
+  while (!valid) {
+    if (Number(scorerInput) === 0) {
+      valid = true;
+      return simpleScore(scorerInput);
+
+    } else if (Number(scorerInput) === 1) {
+      valid = true;
+      return vowelBonusScore(scorerInput);
+
+    } else if (Number(scorerInput) === 2) {
+      valid = true;
+      return oldScrabbleScorer(scorerInput);
+
+    } else {
+      console.output("Please input a matching answer\n")
+      let scorerInput = input.question("Which scoring algorithm would you like to use? 0 for simple scorer, 1 for vowel bonus scorer, 2 for scrabble scorer ");
+    }
+  }
+
+  console.log("Algorithm name: ", scoringAlgorithms[scorerInput].name);
+
+  console.log("ScoreFunction result: ", scoringAlgorithms[scorerInput].ScoreFunction("JavaScript")); // this is test output
+
+}
+
+
+
+
+function transform(pointStructure) {
+
+  for (score in pointStructure) {
+    
+    for (i = 0; i < score.length; i++) {
+      newPointStructure.oldPointStructure.item[i] = score;
+    }
+  }
+}
+
+let newPointStructure = {};
 
 function runProgram() {
-   initialPrompt();
-   
+
+  let numericalScore = initialPrompt();
+
+  let scorerAlgorithm = scorerPrompt();
+  console.log(scorerAlgorithm);
 }
 
 // Don't write any code below this line //
